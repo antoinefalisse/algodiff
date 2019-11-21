1b7ee24063dab82a9138b07e2161a910f95794be
Antoine Falisse <antoinefalisse@gmail.com>
2019-11-21 17:14:31 +0100

    adjust code to compute COT

--- predictiveSimulations_2D/CasADiFunctions/CasADiFunctions_2D.m
+++ predictiveSimulations_2D/CasADiFunctions/CasADiFunctions_2D.m
@@ -127,29 +127,27 @@ f_BackActivationDynamics = ...
     Function('f_BackActivationDynamics',{e_b,a_b},{dadt});
 
 %% Metabolic energy model
-if W.mE ~= 0
-    act_SX          = SX.sym('act_SX',NMuscle,1); % Muscle activations
-    exc_SX          = SX.sym('exc_SX',NMuscle,1); % Muscle excitations
-    lMtilde_SX      = SX.sym('lMtilde_SX',NMuscle,1); % N muscle fiber lengths
-    vMtilde_SX      = SX.sym('vMtilde_SX',NMuscle,1); % N muscle fiber vel
-    vM_SX           = SX.sym('vM_SX',NMuscle,1); % Muscle fiber velocities
-    Fce_SX          = SX.sym('FT_SX',NMuscle,1); % Contractile element forces
-    Fpass_SX        = SX.sym('FT_SX',NMuscle,1); % Passive element forces
-    Fiso_SX         = SX.sym('Fiso_SX',NMuscle,1); % N forces (F-L curve)
-    musclemass_SX   = SX.sym('musclemass_SX',NMuscle,1); % Muscle mass 
-    vcemax_SX       = SX.sym('vcemax_SX',NMuscle,1); % Max contraction vel
-    pctst_SX        = SX.sym('pctst_SX',NMuscle,1); % Slow twitch ratio 
-    Fmax_SX         = SX.sym('Fmax_SX',NMuscle,1); % Max iso forces
-    modelmass_SX    = SX.sym('modelmass_SX',1); % Model mass
-    b_SX            = SX.sym('b_SX',1); % Parameter determining tanh smoothness
-    % Bhargava et al. (2004)
-    [energy_total_sm_SX,Adot_sm_SX,Mdot_sm_SX,Sdot_sm_SX,Wdot_sm_SX,...
-        energy_model_sm_SX] = getMetabolicEnergySmooth2004all(exc_SX,act_SX,...
-        lMtilde_SX,vM_SX,Fce_SX,Fpass_SX,musclemass_SX,pctst_SX,Fiso_SX,...
-        Fmax_SX,modelmass_SX,b_SX);
-    fgetMetabolicEnergySmooth2004all = ...
-        Function('fgetMetabolicEnergySmooth2004all',...
-        {exc_SX,act_SX,lMtilde_SX,vM_SX,Fce_SX,Fpass_SX,musclemass_SX,...
-        pctst_SX,Fiso_SX,Fmax_SX,modelmass_SX,b_SX},{energy_total_sm_SX,...
-        Adot_sm_SX,Mdot_sm_SX,Sdot_sm_SX,Wdot_sm_SX,energy_model_sm_SX});
-end
+act_SX          = SX.sym('act_SX',NMuscle,1); % Muscle activations
+exc_SX          = SX.sym('exc_SX',NMuscle,1); % Muscle excitations
+lMtilde_SX      = SX.sym('lMtilde_SX',NMuscle,1); % N muscle fiber lengths
+vMtilde_SX      = SX.sym('vMtilde_SX',NMuscle,1); % N muscle fiber vel
+vM_SX           = SX.sym('vM_SX',NMuscle,1); % Muscle fiber velocities
+Fce_SX          = SX.sym('FT_SX',NMuscle,1); % Contractile element forces
+Fpass_SX        = SX.sym('FT_SX',NMuscle,1); % Passive element forces
+Fiso_SX         = SX.sym('Fiso_SX',NMuscle,1); % N forces (F-L curve)
+musclemass_SX   = SX.sym('musclemass_SX',NMuscle,1); % Muscle mass 
+vcemax_SX       = SX.sym('vcemax_SX',NMuscle,1); % Max contraction vel
+pctst_SX        = SX.sym('pctst_SX',NMuscle,1); % Slow twitch ratio 
+Fmax_SX         = SX.sym('Fmax_SX',NMuscle,1); % Max iso forces
+modelmass_SX    = SX.sym('modelmass_SX',1); % Model mass
+b_SX            = SX.sym('b_SX',1); % Parameter determining tanh smoothness
+% Bhargava et al. (2004)
+[energy_total_sm_SX,Adot_sm_SX,Mdot_sm_SX,Sdot_sm_SX,Wdot_sm_SX,...
+    energy_model_sm_SX] = getMetabolicEnergySmooth2004all(exc_SX,act_SX,...
+    lMtilde_SX,vM_SX,Fce_SX,Fpass_SX,musclemass_SX,pctst_SX,Fiso_SX,...
+    Fmax_SX,modelmass_SX,b_SX);
+fgetMetabolicEnergySmooth2004all = ...
+    Function('fgetMetabolicEnergySmooth2004all',...
+    {exc_SX,act_SX,lMtilde_SX,vM_SX,Fce_SX,Fpass_SX,musclemass_SX,...
+    pctst_SX,Fiso_SX,Fmax_SX,modelmass_SX,b_SX},{energy_total_sm_SX,...
+    Adot_sm_SX,Mdot_sm_SX,Sdot_sm_SX,Wdot_sm_SX,energy_model_sm_SX});
