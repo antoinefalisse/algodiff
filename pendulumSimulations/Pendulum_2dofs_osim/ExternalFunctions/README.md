9f0500d27100f020f08b55c451b035f7382e7bba
Gil Serrancolí Masferrer <gil.serrancoli@upc.edu>
2020-04-09 12:51:42 +0200

    add OCP with the external function reading an osim file

---
+++ pendulumSimulations/Pendulum_2dofs_osim/ExternalFunctions/README.md
@@ -0,0 +1,17 @@
+External Functions
+==================
+
+When solving the optimal control problems, we use external functions in which
+we perform, among other, inverse dynamics using the Simbody/OpenSim C++ API.
+The external functions are then compiled as dlls from which we create Function instances 
+using CasADi in MATLAB. 
+
+We use custom versions of Simbody and OpenSim to enable the use of algorithmic
+differentiation through CasADi. Information about how to install these libraries, compile
+the cpps, and generate the dlls can be found on this page: https://github.com/antoinefalisse/opensim-core/tree/AD-recorder
+
+In this folder, we release the source code of the external functions (cpps)
+as well as the corresponding dlls. Only the dlls are necessary to run the problems.
+If you work on the different platform (Mac OSX or Ubuntu), you can compile the provide C-code to obtain the equivalent of the windows-dlls. 
+
+Please report any issues: email me at antoine.falisse@kuleuven.be or submit an issue on GitHub.
---
+++ pendulumSimulations/Pendulum_2dofs_osim/batch_OCP_pendulum_2dofs_osim.m
@@ -0,0 +1,54 @@
+% This script loops over all cases with the 2-dof pendulum analyzed in the paper
+%
+% Author: Gil Serrancoli
+% Date: 9/4/2020
+
+%% Run cases with AD-Recorder, (AD-ADOLC), and FD, using mumps and an
+% approximated Hessian of the Lagrangian
+Options.solver='mumps'; % linear solver
+Options.Hessian='approx'; % Hessian calculation scheme
+for der={'AD','FD_F'}
+    Options.derivatives=der{:};    
+    if strcmp(Options.derivatives,'AD')
+        % ADOL-C cases not available
+        for type={'Recorder'} % other option is 'ADOLC' but not available
+            Options.type=type;
+            if strcmp(Options.type,'ADOLC')
+                disp('ADOL-C cases not available')
+                break;
+            end           
+            % 10 initial guesses
+            for i_IG=1:10
+                Options.i_IG=i_IG;
+                OCP_pendulum_2dofs_osim; 
+            end
+        end
+    else
+        Options.type='Recorder';
+        % 10 initial guesses
+        for i_IG=1:10
+            Options.i_IG=i_IG;
+            OCP_pendulum_2dofs; 
+        end
+    end
+end
+
+%% Run cases with AD-Recorder, (the different linear solvers), and the different
+% Hessian calculation schemes (i.e., approximated or exact Hessian).
+Options.derivatives='AD';
+Options.type='Recorder';
+% We are not allowed to share the linear solvers from the HSL collection
+for linsol={'mumps'} % other options are 'ma27','ma57','ma77','ma86','ma97'
+    Options.solver=linsol{:};
+    if ~strcmp(Options.solver,'mumps')
+        disp('Only mumps is available as linear solver')
+        break;
+    end
+    for Hess={'approx','exact'}
+        Options.Hessian=Hess;
+        for i_IG=1:10
+            Options.i_IG=i_IG;
+            OCP_pendulum_2dofs; 
+        end
+    end
+end
